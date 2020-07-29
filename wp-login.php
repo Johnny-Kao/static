2515622048a1f0683f81c8c72aa1509b485d11f1
JK <at12885@gmail.com>
2020-07-30 00:16:51 +0900

    2020-07-29 15:16:42

--- wp-login.php
+++ wp-login.php
@@ -9,11 +9,11 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>登入 &lsaquo; 企鵝旅行手札 &#8212; WordPress</title>
 	<link rel='dns-prefetch' href='//s.w.org' />
-<link rel='stylesheet' id='dashicons-css'  href='/wp-includes/css/dashicons.min.css?ver=5.3.2' type='text/css' media='all' />
-<link rel='stylesheet' id='buttons-css'  href='/wp-includes/css/buttons.min.css?ver=5.3.2' type='text/css' media='all' />
-<link rel='stylesheet' id='forms-css'  href='/wp-admin/css/forms.min.css?ver=5.3.2' type='text/css' media='all' />
-<link rel='stylesheet' id='l10n-css'  href='/wp-admin/css/l10n.min.css?ver=5.3.2' type='text/css' media='all' />
-<link rel='stylesheet' id='login-css'  href='/wp-admin/css/login.min.css?ver=5.3.2' type='text/css' media='all' />
+<link rel='stylesheet' id='dashicons-css'  href='./wp-includes/css/dashicons.min.css?ver=5.3.2' type='text/css' media='all' />
+<link rel='stylesheet' id='buttons-css'  href='./wp-includes/css/buttons.min.css?ver=5.3.2' type='text/css' media='all' />
+<link rel='stylesheet' id='forms-css'  href='./wp-admin/css/forms.min.css?ver=5.3.2' type='text/css' media='all' />
+<link rel='stylesheet' id='l10n-css'  href='./wp-admin/css/l10n.min.css?ver=5.3.2' type='text/css' media='all' />
+<link rel='stylesheet' id='login-css'  href='./wp-admin/css/login.min.css?ver=5.3.2' type='text/css' media='all' />
 	<meta name='robots' content='noindex,noarchive' />
 	<meta name='referrer' content='strict-origin-when-cross-origin' />
 		<meta name="viewport" content="width=device-width" />
@@ -25,7 +25,7 @@
 		<div id="login">
 		<h1><a href="https://tw.wordpress.org/">本站採用 WordPress 建置</a></h1>
 	
-		<form name="loginform" id="loginform" action="/wp-login.php" method="post">
+		<form name="loginform" id="loginform" action="./" method="post">
 			<p>
 				<label for="user_login">使用者名稱或電子郵件地址</label>
 				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" />
@@ -43,48 +43,48 @@
 						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> <label for="rememberme">保持登入</label></p>
 			<p class="submit">
 				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="登入" />
-									<input type="hidden" name="redirect_to" value="/wp-admin/" />
+									<input type="hidden" name="redirect_to" value="./wp-admin/" />
 									<input type="hidden" name="testcookie" value="1" />
 			</p>
 		</form>
 
 					<p id="nav">
-									<a href="/wp-login.php?action=lostpassword">忘記密碼？</a>
+									<a href="./?action=lostpassword">忘記密碼？</a>
 								</p>
 					<script type="text/javascript">
 			function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
 wp_attempt_focus();
 if ( typeof wpOnload === 'function' ) { wpOnload() }		</script>
-				<p id="backtoblog"><a href="/">
+				<p id="backtoblog"><a href="./index.html">
 		&larr; 返回《企鵝旅行手札》		</a></p>
 			</div>
-	<script type='text/javascript' src='/wp-includes/js/jquery/jquery.js?ver=1.12.4-wp'></script>
-<script type='text/javascript' src='/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1'></script>
+	<script type='text/javascript' src='./wp-includes/js/jquery/jquery.js?ver=1.12.4-wp'></script>
+<script type='text/javascript' src='./wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1'></script>
 <script type='text/javascript'>
 /* <![CDATA[ */
-var _zxcvbnSettings = {"src":"\/wp-includes\/js\/zxcvbn.min.js"};
+var _zxcvbnSettings = {"src":".\/wp-includes\/js\/zxcvbn.min.js"};
 /* ]]> */
 </script>
-<script type='text/javascript' src='/wp-includes/js/zxcvbn-async.min.js?ver=1.0'></script>
+<script type='text/javascript' src='./wp-includes/js/zxcvbn-async.min.js?ver=1.0'></script>
 <script type='text/javascript'>
 /* <![CDATA[ */
 var pwsL10n = {"unknown":"\u5bc6\u78bc\u5f37\u5ea6\u672a\u77e5","short":"\u975e\u5e38\u4f4e","bad":"\u4f4e","good":"\u4e2d","strong":"\u9ad8","mismatch":"\u4e0d\u76f8\u7b26"};
 /* ]]> */
 </script>
-<script type='text/javascript' src='/wp-admin/js/password-strength-meter.min.js?ver=5.3.2'></script>
-<script type='text/javascript' src='/wp-includes/js/underscore.min.js?ver=1.8.3'></script>
+<script type='text/javascript' src='./wp-admin/js/password-strength-meter.min.js?ver=5.3.2'></script>
+<script type='text/javascript' src='./wp-includes/js/underscore.min.js?ver=1.8.3'></script>
 <script type='text/javascript'>
 /* <![CDATA[ */
 var _wpUtilSettings = {"ajax":{"url":"\/wordpress\/wp-admin\/admin-ajax.php"}};
 /* ]]> */
 </script>
-<script type='text/javascript' src='/wp-includes/js/wp-util.min.js?ver=5.3.2'></script>
+<script type='text/javascript' src='./wp-includes/js/wp-util.min.js?ver=5.3.2'></script>
 <script type='text/javascript'>
 /* <![CDATA[ */
 var userProfileL10n = {"warn":"\u65b0\u5bc6\u78bc\u5c1a\u672a\u5132\u5b58\u3002","warnWeak":"\u78ba\u8a8d\u4f7f\u7528\u5b89\u5168\u5f37\u5ea6\u8f03\u5f31\u7684\u5bc6\u78bc","show":"\u986f\u793a","hide":"\u96b1\u85cf","cancel":"\u53d6\u6d88","ariaShow":"\u986f\u793a\u5bc6\u78bc","ariaHide":"\u96b1\u85cf\u5bc6\u78bc"};
 /* ]]> */
 </script>
-<script type='text/javascript' src='/wp-admin/js/user-profile.min.js?ver=5.3.2'></script>
+<script type='text/javascript' src='./wp-admin/js/user-profile.min.js?ver=5.3.2'></script>
 	<div class="clear"></div>
 	</body>
 	</html>
